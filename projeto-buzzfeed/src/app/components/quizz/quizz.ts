ddfe7e575049eeaaea19eac8bc9c30c53a22e766
Tulio Perdigão <116309232+tulioperdigao@users.noreply.github.com>
2026-01-02 11:53:20 -0300

    FEAT: Atualiza o template e a lógica do quizz para exibir questões e resultados dinâmicos

--- projeto-buzzfeed/src/app/components/quizz/quizz.ts
+++ projeto-buzzfeed/src/app/components/quizz/quizz.ts
@@ -8,4 +8,19 @@ import { Component } from '@angular/core';
 })
 export class Quizz {
 
+  title:string = "";
+
+  questions:any;
+  questionSelected:any;
+
+  answers:string[] = [];
+  answerSelected:string = "";
+
+  questionIndex:number = 0;
+  questionMaxIndex:number = 0;
+
+  finished:boolean = false;
+
+  constructor() {}
+
 }
